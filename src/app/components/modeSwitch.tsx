a796898e568ec5730c8773c4540d511377086d57
FumioNamai <baggio10.namai@gmail.com>
2024-03-16 17:01:22 +0900

    デプロイエラーを修正

--- src/app/components/modeSwitch.tsx
+++ src/app/components/modeSwitch.tsx
@@ -1,21 +1,21 @@
-import { useModeStore } from "@/store/mode";
-import {
-  Button
-} from "@mui/material";
-import LightModeRoundedIcon from '@mui/icons-material/LightModeRounded';
-import NightlightRoundedIcon from '@mui/icons-material/NightlightRounded'
+// import { useModeStore } from "@/store/mode";
+// import {
+//   Button
+// } from "@mui/material";
+// import LightModeRoundedIcon from '@mui/icons-material/LightModeRounded';
+// import NightlightRoundedIcon from '@mui/icons-material/NightlightRounded'
 
-const ModeSwitch = () => {
-  const mode = useModeStore((state) => (state.mode));
-  const toggleColorMode =  ((state) => (state.toggleColorMode));
+// const ModeSwitch = () => {
+  // const mode = useModeStore((state) => (state.mode));
+  // const toggleColorMode =  ((state) => (state.toggleColorMode));
 
-  return (
-    <Button
-    sx={{padding:0, minWidth:"24px", marginBlock:"12px",}}
-    onClick={toggleColorMode}
-    >{mode === "dark" ? <LightModeRoundedIcon /> : <NightlightRoundedIcon /> }
-    </Button>
-  );
-};
+//   return (
+//     <Button
+//     sx={{padding:0, minWidth:"24px", marginBlock:"12px",}}
+//     onClick={toggleColorMode}
+//     >{mode === "dark" ? <LightModeRoundedIcon /> : <NightlightRoundedIcon /> }
+//     </Button>
+//   );
+// };
 
-export default ModeSwitch;
+// export default ModeSwitch;
