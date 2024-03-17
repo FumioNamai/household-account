d598384b6b4271ec9127471eadda55fb39c4db19
FumioNamai <baggio10.namai@gmail.com>
2024-03-17 18:34:32 +0900

    配色を修正

--- src/app/styles/theme/index.ts
+++ src/app/styles/theme/index.ts
@@ -3,16 +3,32 @@ import { grey } from "@mui/material/colors";
 
 export const lightTheme = createTheme({
   palette: {
-    // primary: { main: "#2a48f3" },
-    // secondary: { main: "#9147FF" },
     mode: "light",
+    text: {
+      primary: grey[900],
+      secondary: grey[800],
+    },
+    background: {
+      paper: grey[100],
+      default: grey[100],
+    },
   },
 });
 
 export const darkTheme = createTheme({
   palette: {
-    // primary: { main: "#9147FF" },
-    // secondary: { main: "#2a48f3" },
     mode: "dark",
+    divider: grey[700],
+    text: {
+      primary: grey[100],
+      secondary: grey[200],
+    },
+    background: {
+      paper: grey[900],
+      default: grey[900],
+    },
+    action: {
+      active: grey[200],
+    },
   },
 });
