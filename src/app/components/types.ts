5d2dde0d89db6e306008977f5126117a193c46fb
FumioNamai <baggio10.namai@gmail.com>
2024-04-19 19:02:31 +0900

    ファイル名の変更を検知させるための処理

--- src/app/components/types.ts
+++
@@ -1,5 +0,0 @@
-export const Types: string[] = [
-  "食品",
-  "雑貨",
-  "その他"
-];
--- src/app/settings/profile/page.tsx
+++
@@ -1,9 +0,0 @@
-import Profile from "@/app/components/profile";
-
-const ProfilePage = () => {
-  return (
-      <Profile />
-  )
-};
-
-export default ProfilePage;
