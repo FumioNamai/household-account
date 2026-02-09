e73b21c39aff3f895e14d47005339cc5cead7c17
FumioNamai <baggio10.namai@gmail.com>
2026-02-09 22:41:26 +0900

    ビルド失敗対応  (supabase.from('profiles') as any)

--- src/app/components/SupabaseListener.tsx
+++ src/app/components/SupabaseListener.tsx
@@ -1,37 +1,45 @@
-"use server"
+"use server";
 
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
-import { Database } from "../../lib/database.types"
-import { cookies } from 'next/headers'
-import Navigation from "@/app/components/Navigation"
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { Database } from "../../lib/database.types";
+import { cookies } from "next/headers";
+import Navigation from "@/app/components/Navigation";
 
 // 認証状態の監視
 const SupabaseListener = async () => {
-  const supabase = createServerComponentClient<Database>({ cookies })
+  const supabase = createServerComponentClient<Database>({ cookies });
 
   // ユーザー情報を取得してナビゲーションに渡す
-  const { data: { user },} = await supabase.auth.getUser()
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
 
   // プロフィール情報を取得する
-  let profile = null
+  let profile = null;
 
   if (user) {
-    const { data: currentProfile } = await supabase.from('profiles').select('*').eq('id',user.id).single()
+    const { data: currentProfile } = await supabase
+      .from("profiles")
+      .select("*")
+      .eq("id", user.id)
+      .single();
 
-    profile = currentProfile
+    profile = currentProfile;
 
     // メールアドレスを変更した場合は、プロフィール情報を更新する
-    if ( currentProfile && currentProfile.email !== user.email ) {
-      const { data:updatedProfiles } = await supabase.from('profiles').update({email:user.email}).match( {
-        id:user.id
-      }).select('*').single()
+    if (currentProfile && currentProfile.email !== user.email) {
+      const { data: updatedProfiles } = await (supabase.from("profiles") as any)
+        .update({ email: user.email })
+        .match({
+          id: user.id,
+        })
+        .select("*")
+        .single();
 
-      profile = updatedProfiles
+      profile = updatedProfiles;
     }
   }
-  return(
-    <Navigation user={user} profile={profile}/>
-  )
-}
+  return <Navigation user={user} profile={profile} />;
+};
 
-export default SupabaseListener
+export default SupabaseListener;
